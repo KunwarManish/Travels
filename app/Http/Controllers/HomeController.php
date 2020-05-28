a2acbea48e37c2b5622c70e26f1fa80c4f97da85
MINGW64 <manish_kunwar@hotmail.com>
2020-05-29 01:01:20 +0545

    Initial Commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function offers()
+    {
+        return view('offers');
+    }
+
+    public function blog()
+    {
+        return view('blog');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+}
